bf232ed80db4e73b0c7e5f95423fc5dc9f19575b
Rocio <rgonzalezt@outlook.com>
2016-03-30 23:38:49 -0300

    agregue el test de negocio

---
+++ Macowins/src/test/java/dominio/NegocioTest.java
@@ -0,0 +1,36 @@
+package dominio;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.Test;
+
+public class NegocioTest {
+	
+	@Test
+	public void testGuardarVentasDia() {
+		Negocio macowins = new Negocio(new BigDecimal(50));
+		Pantalon pant = new Pantalon();
+		pant.setEsImportada(false);
+		Camisa cam = new Camisa();
+		cam.setEsImportada(true);
+		macowins.vender(cam, 2, LocalDate.now());
+		macowins.vender(pant, 1, LocalDate.now());
+		assertEquals(2, macowins.ventasDeLaFecha(LocalDate.now()).size());
+	}
+	
+	@Test
+	public void testMontoVentasDia() {
+		Negocio macowins = new Negocio(new BigDecimal(50));
+		Pantalon pant = new Pantalon();
+		pant.setEsImportada(false);
+		Camisa cam = new Camisa();
+		cam.setEsImportada(true);
+		macowins.vender(cam, 1, LocalDate.now());
+		macowins.vender(pant, 1, LocalDate.now());
+		assertEquals(new BigDecimal(625), macowins.calcularGanancias(LocalDate.now()));
+	}
+
+}
